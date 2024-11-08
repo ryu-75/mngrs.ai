c4efa53ee651cb1f77b172463645a4d5d5ac85bf
Sasha <sasha.lorion@gmail.com>
2024-11-08 16:59:05 +0100

    [UPDATE] - Delete all tags too now

--- project/app/management/commands/delete_all_projects.py
+++ project/app/management/commands/delete_all_projects.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
-from app.models import Project
+from app.models import Project, Tag
 from typing import Dict, Any
+
 class Command(BaseCommand):
     help = "Delete all project in the Project table"
     
@@ -8,7 +9,8 @@ class Command(BaseCommand):
         confirmation = input("Are you sure you want to delete all projects? Type 'yes' to confirm: ")
         
         if confirmation.lower() == 'yes':
-            deleted_count, _ = Project.objects.all().delete()
-            self.stdout.write(self.style.SUCCESS(f"Successfully deleted {deleted_count} projects."))
+            deleted_count_project, _ = Project.objects.all().delete()
+            deleted_count_tag, _ = Tag.objects.all().delete()
+            self.stdout.write(self.style.SUCCESS(f"Successfully deleted {deleted_count_project} projects and {deleted_count_tag} tags"))
         else:
             self.stdout.write(self.style.WARNING("Deletion canceled."))
